62dba47eb75eec8b71a5d7b55d4160fdca4c0f87
fernandoMaia0 <fernandomaia.dev@gmail.com>
2024-12-06 16:11:43 -0300

    correcao erro ao renderizar pag. visualizar_carona e criacao pasta models para salvar classes

--- carona/views.py
+++ carona/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def carona(request):
-    return (request,'visualizar_carona.html')
+    return render(request,'visualizar_carona.html')
