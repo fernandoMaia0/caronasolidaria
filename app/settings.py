f29d90dfed654c1dfa55122de61a39c785b3502e
RaulRodriguesss <santos.raulrodr@gmail.com>
2024-12-09 22:59:52 -0300

    atualização landing page

--- app/settings.py
+++ app/settings.py
@@ -120,8 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
-STATIC_ROOT = 'staticfiles/'
+STATIC_URL = '/staticfiles/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",  # Atualize conforme a estrutura do seu projeto
+]
+STATIC_ROOT = BASE_DIR / "staticfiles_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
