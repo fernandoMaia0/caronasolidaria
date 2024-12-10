2f97acf5c2b314ac04270842e8624c7578d1ccdb
maicon-douglas <maicon.douglas3527@gmail.com>
2024-12-09 22:50:44 -0300

    Alterações locais no settings.py

--- app/settings.py
+++ app/settings.py
@@ -118,10 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",  # Onde seus arquivos estáticos (CSS, JS) estão
+    BASE_DIR / "staticfiles",
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
